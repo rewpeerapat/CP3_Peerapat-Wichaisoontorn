8227a5ce53608ca8b682fa10eb99c9306bd25cf4
rewpeerapat <pwichaisoontoun@gmail.com>
2023-04-16 17:35:19 +0700

    Score

---
+++ Exercise4_Peerapat_W.py
@@ -0,0 +1,9 @@
+FE = 80
+GB = 75.2
+ICS = 90
+CP = 71.5
+print("---Your Score---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
