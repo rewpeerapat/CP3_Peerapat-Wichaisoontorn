d9b6d0b522852d4127ea1c772bdc960282b19030
rewpeerapat <pwichaisoontoun@gmail.com>
2023-04-18 01:41:16 +0700

    HW

---
+++ Lecture54_Peerapat-W.py
@@ -0,0 +1,32 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+def vatCalculate(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+def priceCalculate():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculate(price1 + price2)
+if login():
+    showMenu()
+    userSelected = menuSelect()
+    if (userSelected == 1):
+        totalPrice = int(input("Goods price: "))
+        print(vatCalculate(totalPrice))
+    elif (userSelected == 2):
+        print(priceCalculate())
+else:
+    print("Invalid username or password.")
