25ad74047dfcf884be970858c0f6d215a510935c
rewpeerapat <pwichaisoontoun@gmail.com>
2023-04-19 22:07:24 +0700

    Create Lecture71_Peerapat-W.py

---
+++ Lecture71_Peerapat-W.py
@@ -0,0 +1,18 @@
+menuList = []
+priceList = []
+def showBill():
+    print("----My bill----")
+    for number in range(len(menuList)):
+        print(menuList[number],priceList[number])
+    print("Total :",sum(priceList),"THB")
+
+while True:
+    menuName = input("Please Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append(menuName)
+        priceList.append(float(menuPrice))
+
+showBill()
